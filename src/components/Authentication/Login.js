db9c7a08c1c8fdc34b7ccfda7ee8fd3b3cb647da
ashraf9932 <ashraf.neizk@gmail.com>
2024-09-07 02:48:00 +0300

    fix useAuth import in login.js

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import { useAuth } from '../../hooks/useAuth';
+import useAuth from '../../hooks/useAuth';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
