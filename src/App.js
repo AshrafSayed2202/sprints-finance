9cbe303e96f049d1b5702feaef767d60a8f5fa28
ashraf9932 <ashraf.neizk@gmail.com>
2024-09-07 02:44:21 +0300

    App + index Done

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { PortfolioProvider } from './context/PortfolioContext';
 import { AuthProvider } from './context/AuthContext';
-import { ERPProvider } from './context/ERPContext';
-import Navbar from './components/common/Navbar';
-import Loader from './components/common/Loader';
-
-// Use React.lazy for dynamic imports
-const Home = React.lazy(() => import('./pages/Home'));
-const InventoryManagement = React.lazy(() => import('./pages/InventoryManagement'));
-const FinanceManagement = React.lazy(() => import('./pages/FinanceManagement'));
-const SalesManagement = React.lazy(() => import('./pages/SalesManagement'));
-const LoginPage = React.lazy(() => import('./pages/LoginPage'));
+import AppRoutes from './pages/routes';
 
 const App = () => {
   return (
     <AuthProvider>
-      <ERPProvider>
-        <Router>
-          <Navbar />
-          <Suspense fallback={<Loader />}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/inventory" element={<InventoryManagement />} />
-              <Route path="/finance" element={<FinanceManagement />} />
-              <Route path="/sales" element={<SalesManagement />} />
-            </Routes>
-          </Suspense>
-        </Router>
-      </ERPProvider>
+      <PortfolioProvider>
+        <AppRoutes />
+      </PortfolioProvider>
     </AuthProvider>
   );
 };
